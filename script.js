aba706695a63e6ec28feb414081be76af5cf1109
rezha4 <rezha.bahari@gmail.com>
2023-08-04 09:25:20 +0700

    using js to draw the pixels on board
    still trying to figure out how to evenly distribute the pixels accross the board

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//
+const container = document.querySelector(".container");
+
+let size = 16*16;
+
+console.log(size);
+
+do {
+    const pixels = document.createElement("div");
+    container.appendChild(pixels);
+    console.log("tes");
+    size--;
+} while (size != 0);
